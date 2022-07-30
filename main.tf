fd4ca7b39159b5633eec51117142419702f0cd9a
Laurence Nairne <laurencenairne@gmail.com>
2022-07-30 23:13:55 +0100

    Added VM and various other resource declarations

--- main.tf
+++ main.tf
@@ -55,9 +55,9 @@ resource "azurerm_network_security_rule" "laurence-dev-nsr" {
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "192.168.0.57/32"
+  source_port_range           = "22"
+  destination_port_range      = "22"
+  source_address_prefix       = "192.168.0.0/24"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.laurence-rg.name
   network_security_group_name = azurerm_network_security_group.laurence-nsg.name
