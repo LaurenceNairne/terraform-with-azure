561ec9f8cc0649ed016d8f4a492196d174814637
Laurence Nairne <laurencenairne@gmail.com>
2022-08-14 13:48:38 +0100

    It didn't like data being passed into variables...

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "client_id" {
   description = "The Azure service principal ID"
   type        = string
-  default     = data.azurerm_client_config.current.client_id
+  default     = ""
 }
 
 variable "subscription_id" {
   description = "The Azure subscription the app sits under"
   type        = string
-  default     = data.azurerm_client_config.current.subscription_id
+  default     = ""
 }
 
 variable "tenant_id" {
   description = "The Azure tenant the app is associated with"
   type        = string
-  default     = data.azurerm_client_config.current.tenant_id
+  default     = ""
 }
 
 variable "client_secret" {
   description = "The Azure service principal password/secret"
   type        = string
-  default     = data.azurerm_client_config.current.client_secret
+  default     = ""
 }
